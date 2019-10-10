ca0aa58f20e77a4634a7a4d7f392a0242e9fb5ca
Mark Alston <malston@pivotal.io>
2019-10-10 09:33:37 -0600

    Change script for getting the ingress url

--- microservice-istio-demo/ingress-url.sh
+++ microservice-istio-demo/ingress-url.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-IP=$(minikube ip) 
-PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+# IP=$(minikube ip)
+# PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+
+IP=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
 echo http://$IP:$PORT/
